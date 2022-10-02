0c5d3f71ccdfd43023304ad591e573530862c57b
Piotr Grabkowski <blaisepascal1@wp.pl>
2022-10-03 01:21:41 +0200

    changing file-upload-component

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -14,9 +14,12 @@ import { GalleryService } from '../gallery.service';
 export class FileUploadComponent implements OnInit {
 
   isChosenFileVisible: boolean = false;
+  spinnerDisplayed: boolean = false;
   chosenFileName: string;
+  chosenFileSize: number;
   chosenFile : File;
   description : string = '';
+
 
   constructor( private router : Router, 
     private galleryService : GalleryService,
@@ -32,13 +35,57 @@ export class FileUploadComponent implements OnInit {
 
   onFileInputChage(event){
 
+    const that = this;
+
+
      const file: File = event.target.files[0];
 
+     const container = document.querySelector(".img-div");
+     const containerChildren = container.children;
+     Array.from(containerChildren).forEach(e=>{
+        if(e.tagName==='IMG' || e.tagName === 'SPAN'){
+
+          e.remove();
+        }
+
+     });
+     const nameSpan = document.createElement("span");
+     const sizeSpan = document.createElement("span");
+     this.spinnerDisplayed=true;
+
      this.chosenFileName = file.name;
+     this.chosenFileSize = file.size;
+     nameSpan.innerText = 'Wybrano plik: ' + this.chosenFileName;
+     sizeSpan.innerText = 'Rozmiar pliku: ' + Math.round(this.chosenFileSize/1000) +'KB';
+     
      this.chosenFile = file;
+    
+     
+     const fileReader = new FileReader();
+     fileReader.readAsDataURL(file);
+     fileReader.onload = function(){
+      const img = new Image();
+      img.setAttribute('width','90%');
+      img.setAttribute('height', '50%');
+
+      if(typeof fileReader.result ==='string'){
+      img.src = fileReader.result;
+      }
+      that.spinnerDisplayed = false;
+
+      container.appendChild(img);
+      container.appendChild(nameSpan);
+      container.appendChild(sizeSpan);
+      
 
 
-     this.isChosenFileVisible = true;
+     };
+
+  
+     
+
+
+     
 
 
   }
